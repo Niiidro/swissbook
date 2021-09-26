80220611ca9dd489848abd92bf389e34845acbda
alessandro-holik <alessandro.holik@outlook.com>
2021-09-26 19:28:13 +0200

    backend

---
+++ backend/src/test/kotlin/ch/swissbook/swissbook/SwissbookApplicationTests.kt
@@ -0,0 +1,13 @@
+package ch.swissbook.swissbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwissbookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
