4ed2f1552b870b46b33c1edaf7581c6f51e3122e
alessandro-holik <alessandro.holik@outlook.com>
2021-10-04 12:49:33 +0200

    improved password check

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,13 @@
 const Joi = require('joi');
 const Mongoose = require('mongoose');
+const PasswordValidator = require('password-validator');
 
 const User = Mongoose.model('User', new Mongoose.Schema({
 
     name: {
         type: String,
         required: false,
-        minlenght: 5,
+        minlenght: 1,
         maxlength: 50
     },
     email: {
@@ -24,14 +25,33 @@ const User = Mongoose.model('User', new Mongoose.Schema({
     }
 }));
 
+function validatePassword(req, res, next) {
+    const schema = new PasswordValidator();
+    // prettier-ignore
+    schema
+        .is().min(8)
+        .has().uppercase()
+        .has().lowercase()
+        .has().digits()
+        .has().symbols();
+    if (schema.validate(req.body.password)) {
+        next();
+    } else {
+        res.status(409).send({
+            'success':false , message:'The Password needs to be at least 8 characters with one uppercase, one lowercase, one digit and one symbol '
+        });
+    }
+}
+
 function validateUser(user) {
     const schema = Joi.object({
-        name: Joi.string().min(5).max(50).required(),
+        name: Joi.string().min(1).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
+        password: Joi.string().min(1).max(50).required(),
     });
     return schema.validate(user)
 }
 
 exports.User = User;
-exports.validate = validateUser;
+exports.validateUser = validateUser;
+exports.validatePassword = validatePassword;
